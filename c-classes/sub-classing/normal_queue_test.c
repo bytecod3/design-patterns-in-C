e83d7934ddee1ce76e46e9ee61098f4e016bea71
bytecod3 <emwiti658@gmail.com>
2025-11-19 09:38:00 +0300

    STARTS normal queue test

---
+++ c-classes/sub-classing/normal_queue_test.c
@@ -0,0 +1,19 @@
+/**
+ * This file tests the normal queue
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+int main(void) {
+    int j, k, h, t;
+
+    /* test normal queue */
+    Queue* myQ;
+    myQ = Queue_create();
+    k = 1000;
+
+    for(j = 0; j < QUEUE_SIZE; j++) {
+        
+    }
+}
